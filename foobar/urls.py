858ba3e25ec20c8aaa3e60940116a482a9d0c5c7
Xorast <xavier.astor.fontaine@gmail.com>
2020-01-10 11:34:45 +0000

    Updated everything

---
+++ foobar/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import get_first_page, get_2020_page, read_movies, create_movie, update_movie
+
+urlpatterns = [
+    path('hello', get_first_page),
+    path('year-2020', get_2020_page),
+    path('read-movies', read_movies),
+    path('create-movie', create_movie),
+    path('update-movie', update_movie)
+]
---
+++ foobar/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Movies
+from .forms import MoviesForm
+
+
+# Create your views here.
+def get_first_page(request):
+    return render(request, "foobar/test.html")
+
+
+def get_2020_page(request):
+    names = ["Darragh", "Marta", "Rory", "Xav"]    
+    return render(request, "foobar/year-2020.html",
+                  {'year': "2020", "names": names})
+
+
+def read_movies(request):
+    movies = Movies.objects.all()
+    return render(request, "foobar/movies.html", {"movies": movies})
+
+
+def create_movie(request):
+
+    if request.method == "GET":
+        form = MoviesForm()
+        return render(request, "foobar/movie-create.html", {'form': form})
+
+    if request.method == "POST":
+
+        form = MoviesForm(request.POST)
+
+        if form.is_valid():
+            movie = form.save(commit=False)
+            movie.save()
+
+            return render(request, "foobar/movie-creation-confirmed.html")
+
+
+def update_movie(request):
+
+    if request.method == "GET":
+        movie = get_object_or_404(Movies, pk=1)
+        form = MoviesForm(instance=movie)
+        return render(request, "foobar/movie-update.html", {'form': form})
+
+    if request.method == 'POST':
+        movie = get_object_or_404(Movies, pk=1)
+        form = MoviesForm(request.POST, instance=movie)
+        form.save()
+        return render(request, "foobar/movie-creation-confirmed.html")
+
+
